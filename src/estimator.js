ae34494518d37a50eac1adef8ce958af6883e9f1
mpiranha <chukwubuezeokeke@gmail.com>
2020-04-12 22:10:41 +0100

    estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,44 +1,3 @@
-
-// let demo = {
-//     region: {
-//         name: "Africa",
-//         avgAge: 19.7,
-//         avgDailyIncomeInUSD: 5,
-//         avgDailyIncomePopulation: 0.71
-//     },
-//     periodType: "days",
-//     timeToElapse: 58,
-//     reportedCases: 674,
-//     population: 66622705,
-//     totalHospitalBeds: 1380614
-// }
-
-const covid19ImpactEstimator = (data) => {
-
-
-    return {
-        data: data,
-        impact: {
-            currentlyInfected: () => Math.trunc(data.reportedCases * 10),
-            infectionsByRequestedTime: () => Math.trunc(this.currentlyInfected * (2 ** factor(data.periodType, data.timeToElapse))),
-            severeCasesByRequestedTime: () => Math.trunc(0.15 * this.infectionsByRequestedTime),
-            hospitalBedsByRequestedTime: () => Math.trunc((data.totalHospitalBeds * 0.35) - this.severeCasesByRequestedTime),
-            casesForICUByRequestedTime: () => Math.trunc(0.05 * this.infectionsByRequestedTime),
-            casesForVentilatorsByRequestedTime: () => Math.trunc(0.02 * this.infectionsByRequestedTime),
-            dollarsInFlight: () => Math.trunc((this.infectionsByRequestedTime * data.avgDailyIncomePopulation * avgDailyIncomeInUSD) / 30)
-        },
-        severeImpact: {
-            currentlyInfected: () => reportedCases * 50,
-            infectionsByRequestedTime: () => Math.trunc(this.currentlyInfected * (2 ** factor(data.periodType, data.timeToElapse))),
-            severeCasesByRequestedTime: () => Math.trunc(0.15 * this.infectionsByRequestedTime),
-            hospitalBedsByRequestedTime: () => Math.trunc((data.totalHospitalBeds * 0.35) - this.severeCasesByRequestedTime),
-            casesForICUByRequestedTime: () => Math.trunc(0.05 * this.infectionsByRequestedTime),
-            casesForVentilatorsByRequestedTime: () => Math.trunc(0.02 * this.infectionsByRequestedTime),
-            dollarsInFlight: () => Math.trunc((this.infectionsByRequestedTime * data.avgDailyIncomePopulation * avgDailyIncomeInUSD) / 30)
-        }
-    }
-};
-
 const getDay = (periodType, value) => {
     let days = 0;
     if (periodType == "weeks") {
@@ -68,6 +27,34 @@ const factor = (periodType, value) => {
 
     return factor;
 }
+
+const covid19ImpactEstimator = (data) => {
+
+
+    return {
+        data: data,
+        impact: {
+            currentlyInfected: () => Math.trunc(data.reportedCases * 10),
+            infectionsByRequestedTime: () => Math.trunc(this.currentlyInfected * (2 ** factor(data.periodType, data.timeToElapse))),
+            severeCasesByRequestedTime: () => Math.trunc(0.15 * this.infectionsByRequestedTime),
+            hospitalBedsByRequestedTime: () => Math.trunc((data.totalHospitalBeds * 0.35) - this.severeCasesByRequestedTime),
+            casesForICUByRequestedTime: () => Math.trunc(0.05 * this.infectionsByRequestedTime),
+            casesForVentilatorsByRequestedTime: () => Math.trunc(0.02 * this.infectionsByRequestedTime),
+            dollarsInFlight: () => Math.trunc((this.infectionsByRequestedTime * data.avgDailyIncomePopulation * avgDailyIncomeInUSD) / 30)
+        },
+        severeImpact: {
+            currentlyInfected: () => reportedCases * 50,
+            infectionsByRequestedTime: () => Math.trunc(this.currentlyInfected * (2 ** factor(data.periodType, data.timeToElapse))),
+            severeCasesByRequestedTime: () => Math.trunc(0.15 * this.infectionsByRequestedTime),
+            hospitalBedsByRequestedTime: () => Math.trunc((data.totalHospitalBeds * 0.35) - this.severeCasesByRequestedTime),
+            casesForICUByRequestedTime: () => Math.trunc(0.05 * this.infectionsByRequestedTime),
+            casesForVentilatorsByRequestedTime: () => Math.trunc(0.02 * this.infectionsByRequestedTime),
+            dollarsInFlight: () => Math.trunc((this.infectionsByRequestedTime * data.avgDailyIncomePopulation * avgDailyIncomeInUSD) / 30)
+        }
+    }
+};
+
+
 
 // 111 111 111 111 1
 
